5c46a31f89fbbb89fcbf1caab41f0bb35d42a1ab
Isaias Pinto <85035512+IsaiasSPinto@users.noreply.github.com>
2021-07-25 21:30:12 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import GlobalStyles from './GlobalStyles';
 
